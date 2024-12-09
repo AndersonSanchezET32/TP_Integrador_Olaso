2adb35577cd541529e8ccfd48438197e1c07fe71
AndersonSanchezET32 <andersonsanchezlopezet32@gmail.com>
2024-12-08 21:22:24 -0300

    Delete paginaweb directory

--- paginaweb/src/requirements/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = new mysqli("localhost", "root", "", "manejostock");
-
-    if ($conexion->connect_error) {
-        die("Error en la conexión: " . $conexion->connect_error);
-
-        
-    }    
-?>
